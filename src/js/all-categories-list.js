7df948e84cfbe234a6fd3098bc7e296b5d1251fe
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-05-16 19:43:55 +0300

    js fixed

--- src/js/all-categories-list.js
+++ src/js/all-categories-list.js
@@ -4,31 +4,47 @@ const allBooks = document.querySelector('.all-categories');
 
 getTopBooks().then(function (response) {
   console.log('top book', response);
-  allBooks.innerHTML = title(response) + book(response);
-  // allBooks.innerHTML =
+  allBooks.innerHTML = renderMarkup(response);
 });
 
-function title(arr) {
-  return arr
-    .map(({ list_name }) => {
-      return `<p class="best-sellers-categori">${list_name}</p>`;
-    })
-    .join(' ');
-}
-
-function book(arr) {
-  return arr.map(({ books }) => {
-    return books
-      .map(({ book_image, title, author }) => {
-        return `
-    <ul class="best-sellers-list">
+function renderMarkup(data) {
+  const renderedItems = data.map(obj => {
+    const innerMarkup = obj.books.map(book => {
+      return `<ul class="best-sellers-list">
       <li class="best-sellers-item">
-        <img src="${book_image}" alt="" />
-        <h3 class="best-sellers-name">${title}</h3>
-        <p class="best-sellers-author">${author}</p>
+        <img src="${book.book_image}" alt="" />
+        <h3 class="best-sellers-name">${book.title}</h3>
+        <p class="best-sellers-author">${book.author}</p>
       </li>
     </ul>`;
-      })
-      .join(' ');
+    });
+    const mergedInnerMarkup = innerMarkup.join('');
+    return `<div>
+              <h2> ${obj.list_name}</h2>
+              ${mergedInnerMarkup}
+            </div>`;
   });
+  const finalMarkup = renderedItems.join('');
+  return finalMarkup;
 }
+
+// function title(arr) {
+
+// }
+
+// function book(arr) {
+//   return arr.map(({ books }) => {
+//     return books
+//       .map(({ book_image, title, author }) => {
+//         return `
+//     <ul class="best-sellers-list">
+//       <li class="best-sellers-item">
+//         <img src="${book_image}" alt="" />
+//         <h3 class="best-sellers-name">${title}</h3>
+//         <p class="best-sellers-author">${author}</p>
+//       </li>
+//     </ul>`;
+//       })
+//       .join(' ');
+//   });
+// }
