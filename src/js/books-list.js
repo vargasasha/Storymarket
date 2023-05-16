30ecb4dd8c1da1c080a8ca587510e2c63a3ab115
Oleksandr Varga <vargasasha@gmail.com>
2023-05-16 17:51:50 +0300

    add css style

--- src/js/books-list.js
+++ src/js/books-list.js
@@ -1,30 +1,51 @@
 import { getBookByCategory } from './main-fetch';
 
 const list = document.querySelector('.js-list');
+const categoryTitle = document.querySelector('.js-category-books-title');
 const listCategory = document.querySelector('.categories-list');
+
 
 listCategory.addEventListener('click', handlerAllCategoryBooks);
 
 function handlerAllCategoryBooks(evt) {
-    evt.preventDefault();
-    categoryBooks(evt.target.textContent);
+  evt.preventDefault();
+  categoryBooks(evt.target.textContent);
 }
 
 function cardMarkup(array) {
   return array
     .map(
       ({ book_image, title, author, _id }) =>
-        `<li id="${_id}"><a href=""><img src="${book_image}" alt="${title}"><h2>${title}</h2><p>${author}</p></a></li`
+        `<li class="category-item" id="${_id}">
+        <a href="">
+          <div class="book-card-wrap">
+            <img src="${book_image}" alt="${title}" />
+            <p class="card-up-text"></p>
+          </div>
+          <div>
+            <h3 class="book-card-title">${title}</h3>
+            <p class="book-card-author">${author}</p>
+          </div>
+        </a>
+      </li>`
     )
     .join('');
+}
+
+function titleMarkup(title) {
+  const wordArray = title.split(' ');
+  const lastWord = wordArray[wordArray.length - 1];
+  const firstTitle = wordArray.slice(0, wordArray.length - 1).join(' ') + ' ';
+
+  categoryTitle.innerHTML = `<h2 class="category-title-begin">${firstTitle}<span class="category-title-end">${lastWord}</span></h2>`;
 
 }
 
 function categoryBooks(category) {
 
+  titleMarkup(category);
+
   getBookByCategory(category).then(arr => {
-
     list.innerHTML = cardMarkup(arr);
-
   });
 }
