2c4f2f61a8e52f0fba3f1c41d075b6c5fbe6d3c9
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-05-16 19:16:45 +0300

    дода розмітку книг

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import '../src/js/main-fetch';
 import '../src/js/markup-list-category';
+import '../src/js/all-categories-list';
--- src/js/all-categories-list.js
+++ src/js/all-categories-list.js
@@ -1,5 +1,34 @@
 import { getTopBooks } from './main-fetch';
 
+const allBooks = document.querySelector('.all-categories');
+
 getTopBooks().then(function (response) {
   console.log('top book', response);
+  allBooks.innerHTML = title(response) + book(response);
+  // allBooks.innerHTML =
 });
+
+function title(arr) {
+  return arr
+    .map(({ list_name }) => {
+      return `<p class="best-sellers-categori">${list_name}</p>`;
+    })
+    .join(' ');
+}
+
+function book(arr) {
+  return arr.map(({ books }) => {
+    return books
+      .map(({ book_image, title, author }) => {
+        return `
+    <ul class="best-sellers-list">
+      <li class="best-sellers-item">
+        <img src="${book_image}" alt="" />
+        <h3 class="best-sellers-name">${title}</h3>
+        <p class="best-sellers-author">${author}</p>
+      </li>
+    </ul>`;
+      })
+      .join(' ');
+  });
+}
